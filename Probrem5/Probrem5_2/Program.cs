8570e08f9bf0124850a3d1d114289099e5bab9cd
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-14 15:50:03 +0900

    Probrem5_2 added

---
+++ Probrem5/Probrem5_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Probrem5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] d = { 0.2, -5.1, 3.2, 1.8 };
+
+            for (int i = 0; i < d.Length; i++){
+                Console.Write("d[{0}]={1} ", i, d[i]);
+            }
+        }
+    }
+}
