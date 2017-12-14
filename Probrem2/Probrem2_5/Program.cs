39c12360b9665a0df0057c5473aa9b9e0d1953d2
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-14 13:23:22 +0900

    Probrem2 added

---
+++ Probrem2/Probrem2_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1, str2;
+            Console.Write("文字列１を入力:");
+            str1 = Console.ReadLine();
+            Console.Write("文字列２を入力");
+            str2 = Console.ReadLine();
+            Console.WriteLine("文字列１＋文字列２＝{0}", str1 + str2);
+        }
+    }
+}
