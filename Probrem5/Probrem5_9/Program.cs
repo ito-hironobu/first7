e47aa3cd94613c4bcdd098e26ac76967c6b564d5
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-15 12:03:12 +0900

    Probrem5_9 added

---
+++ Probrem5/Probrem5_9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Probrem5_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[10];
+            int rnd_max = 100;
+            int rnd_min = 0;
+            Random rnd = new Random();
+
+            for (int i = 0; i < data.Length; i++){
+                data[i] = rnd.Next(rnd_min, rnd_max + 1);
+                Console.Write(data[i] + " ");
+            }
+
+            string str_over = "50以上の数：";
+            string str_under = "50未満の数：";
+            for (int i = 0; i < data.Length; i++){
+                if(data[i] >= 50){
+                    str_over += data[i] + " ";
+                }else{
+                    str_under += data[i] + " ";
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(str_over);
+            Console.WriteLine(str_under);
+        }
+    }
+}
