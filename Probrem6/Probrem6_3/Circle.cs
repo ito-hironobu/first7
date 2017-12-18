e201e65a2d85b3279a73494b2547c9a4e8c3a1ff
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-18 11:52:37 +0900

    Probre6_3 added

---
+++ Probrem6/Probrem6_3/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem6_3
+{
+    public class Circle
+    {
+        public double r;
+
+        public double Circumference()
+        {
+            return 2 * 3.14 * r;
+        }
+
+        public double Area()
+        {
+            return r * r * 3.14;
+        }
+    }
+}
