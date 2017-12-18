b666396848a79ae7fedfb45e8824805e9e81a1f0
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-18 11:30:48 +0900

    Probre6_1 added

---
+++ Probrem6/Probrem6_1/EmptyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Probrem6_1
+{
+    class Person
+    {
+        //  名前（フィールド）
+        public string name = "";
+        //  年齢（フィールド）
+        public int age = 0;
+        //  情報の表示（メソッド）
+        public void ShowAgeAndName()
+        {
+            Console.WriteLine("名前：{0} 年齢：{1}", name, age);
+        }
+        //  情報の設定
+        public void SetAgeAndName(string name, int a)
+        {
+            this.name = name;
+            age = a;
+        }
+    }
+}
---
+++ Probrem6/Probrem6_1/MinMax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem6_1
+{
+    public class MinMax
+    {
+        public int Max(int a, int b, int c)
+        {
+            int max = a;
+            if(b > max){
+                max = b;
+            }
+            if(c > max){
+                max = c;
+            }
+            return max;
+        }
+        public int Min(int a, int b, int c){
+            int min = a;
+            if (b < min)
+            {
+                min = b;
+            }
+            if (c < min)
+            {
+                min = c;
+            }
+            return min;
+        }
+    }
+}
