bc95fbcb581bf57e2ba8bbc955de0f8db4f02fef
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-18 15:41:00 +0900

    Probre8_3 added

---
+++ Probrem8/Probrem8_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem8_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("0以上の整数を入力：");
+            ulong num = ulong.Parse(Console.ReadLine());
+
+            ulong factorial_num = 1;
+            if(num > 0){
+                for (ulong i = num; i > 0; i--){
+                    factorial_num *= i;
+                }
+                Console.WriteLine(num + "の階乗 = " + factorial_num);
+            }else if(num == 0){
+                Console.WriteLine(num + "の階乗 = " + factorial_num);
+            }else{
+                Console.WriteLine("0以上の整数を入力してください");
+            }
+        }
+    }
+}
