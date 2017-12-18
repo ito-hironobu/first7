482eb31f827879a17d0496a0bf7f2396b0b50e9b
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-18 19:02:02 +0900

    Probre8_9 added

---
+++ Probrem8/Probrem8_9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem8_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10000以下の完全数：");
+            for (int i = 1; i <= 10000; i++){
+                int sum = 0;
+                for (int j = 1; j <= i / 2; j++){
+                    if(i % j == 0){
+                        sum += j;
+                    }
+                }
+                if(sum == i){
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
