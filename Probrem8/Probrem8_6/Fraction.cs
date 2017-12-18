bf301e8be7284fe3835bc8529d7246db5032b9d6
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-18 18:51:59 +0900

    Probre8_8 added

--- Probrem8/Probrem8_6/Fraction.cs
+++ Probrem8/Probrem8_6/Fraction.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Probrem8_6
 {
     public class Fraction
     {
+        private int numerator; // 分子
+        private int denominator; // 分母
+
         public Fraction()
         {
         }
---
+++ Probrem8/Probrem8_8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem8_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rnd_min = 1;
+            int rnd_max = 1000;
+            Random rnd = new Random();
+            int num = rnd.Next(rnd_min, rnd_max + 1);
+
+            Console.WriteLine(num);
+            Console.Write("約数：");
+
+            for (int i = 1; i <= num; i++){
+                if(num % i == 0){
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
