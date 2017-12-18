5cdf642b4673c42576bf38b90563c0ffed1f28aa
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-18 12:54:06 +0900

    Probre7_1 added

---
+++ Probrem7/Probrem7_1/Data.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem7_1
+{
+    public class Data
+    {
+        private int number = 0;
+        private string comment = "";
+
+        public int Number
+        {
+            set { number = value; }
+            get { return number; }
+        }
+
+        public string Comment{
+            set { comment = value; }
+            get { return comment; }
+        }
+    }
+}
---
+++ Probrem7/Probrem7_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data d = new Data();
+            d.Number = 100;
+            d.Comment = "Programing C#";
+            Console.WriteLine("number = " + d.Number);
+            Console.WriteLine("comment = " + d.Comment);
+        }
+    }
+}
