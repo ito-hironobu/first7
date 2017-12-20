5e5776b6703c2c8680d0e0bf6047088cd52ee54e
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-20 12:52:24 +0900

    Probre8_6 added

--- Probrem8/Probrem8_6/Fraction.cs
+++ Probrem8/Probrem8_6/Fraction.cs
@@ -8,11 +8,26 @@ namespace Probrem8_6
 {
     public class Fraction
     {
+        private int integer; // 整数部分
         private int numerator; // 分子
         private int denominator; // 分母
 
-        public Fraction()
+        public int I
         {
+            set { integer = value; }
+            get { return integer; }
+        }
+
+        public int N
+        {
+            set { numerator = value; }
+            get { return numerator; }
+        }
+
+        public int D
+        {
+            set { denominator = value; }
+            get { return denominator; }
         }
     }
 }
--- Probrem8/Probrem8_6/Program.cs
+++ Probrem8/Probrem8_6/Program.cs
@@ -10,7 +10,63 @@ namespace Probrem8_6
     {
         static void Main(string[] args)
         {
-            
+            Random rnd = new Random();
+            int rnd_n_min = 1;
+            int rnd_d_min = 2;
+            int rnd_max = 10;
+
+            Fraction f1 = new Fraction();
+            Fraction f2 = new Fraction();
+            f1.N = rnd.Next(rnd_n_min, rnd_max + 1);
+            f1.D = rnd.Next(rnd_d_min, rnd_max + 1);
+            f2.N = rnd.Next(rnd_n_min, rnd_max + 1);
+            f2.D = rnd.Next(rnd_d_min, rnd_max + 1);
+
+            Fraction f = AddFraction(f1, f2);
+
+            // 計算結果表示
+            Console.Write("{0}/{1} + {2}/{3} = ", f1.N, f1.D, f2.N, f2.D);
+            if(f.I == 0){ // 整数部分が0
+                if(f.N == 0){ // 分数部分なし
+                    Console.WriteLine(f.I); // 表示）0
+                }else{ // 分数部分あり
+                    Console.WriteLine(f.N + "/" + f.D); // 表示例）1/3
+                }
+            }else{ // 整数部分あり
+                Console.Write(f.I); // 整数部分表示
+                if(f.N != 0){ // 分数部分あり
+                    Console.WriteLine("+({0}/{1})", f.N, f.D); // 表示例）1+(1/3)
+                }
+            }
+        }
+
+        static Fraction AddFraction(Fraction f1, Fraction f2)
+        {
+            Fraction f = new Fraction();
+            f.N = (f1.N * f2.D) + (f2.N * f1.D); // 通分後の分子の足し算
+            f.D = f1.D * f2.D; // 通分後の分母
+            // 約分と帯分数化したものを返す
+            return AboutAndBand(f);
+        }
+
+        static Fraction AboutAndBand(Fraction f)
+        {
+            f.I = f.N / f.D; // 整数部分
+            f.N %= f.D; // 小数部分
+            if (f.N == 0) { return f; } // fが整数になる（分子が0）ならリターン
+
+            // 約分処理
+            for (int num = 2; num <= f.D; num++){
+                while(true){
+                    if (f.N % num == 0 && f.D % num == 0){
+                        f.N /= num;
+                        f.D /= num;
+                    }else{
+                        break;
+                    }
+                }
+            }
+            return f;
         }
     }
 }
