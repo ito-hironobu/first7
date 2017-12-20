785f240828349df70f14db3f163d9c5b40d4379b
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-20 14:09:04 +0900

    Probre8_6 modified2

--- Probrem8/Probrem8_6/Fraction.cs
+++ Probrem8/Probrem8_6/Fraction.cs
@@ -8,45 +8,60 @@ namespace Probrem8_6
 {
     public class Fraction
     {
-        private int integer; // 整数部分
-        private int numerator; // 分子
+        private int integer = 0; // 整数部分
+        private int numerator = 0; // 分子
         private int denominator; // 分母
 
         public string GetFracString()
         {
             string f_str = "";
-            if (integer == 0){ // 整数部分が0
-                if (numerator == 0){ // 分数部分なし
+            if (integer == 0)
+            { // 整数部分が0
+                if (numerator == 0)
+                { // 分数部分なし
                     f_str += integer; // 0代入
-                }else{ // 分数部分あり
+                }
+                else
+                { // 分数部分あり
                     f_str += numerator + "/" + denominator; // 例）1/3代入
                 }
-            }else{ // 整数部分あり
+            }
+            else
+            { // 整数部分あり
                 f_str += integer; // 整数部分代入
-                if (numerator != 0){ // 分数部分あり
+                if (numerator != 0)
+                { // 分数部分あり
                     f_str += "(" + numerator + "/" + denominator + ")"; // 例）(1/3)追加代入
                 }
             }
             return f_str;
         }
 
-        public void AboutAndBand()
+        // 約分
+        public void About()
         {
-            integer = numerator / denominator; // 整数部分
-            numerator %= denominator; // 小数部分
-            if (numerator == 0) { return; } // fが整数になる（分子が0）ならリターン
-
-            // 約分処理
-            for (int num = 2; num <= denominator; num++){
-                while (true){
-                    if (numerator % num == 0 && denominator % num == 0){
+            for (int num = 2; num <= denominator; num++)
+            {
+                while (true)
+                {
+                    if (numerator % num == 0 && denominator % num == 0)
+                    {
                         numerator /= num;
                         denominator /= num;
-                    }else{
+                    }
+                    else
+                    {
                         break;
                     }
                 }
             }
+        }
+
+        // 帯分数化
+        public void Band()
+        {
+            integer += numerator / denominator; // 整数部分
+            numerator %= denominator; // 小数部分
         }
 
         public int I
--- Probrem8/Probrem8_6/Program.cs
+++ Probrem8/Probrem8_6/Program.cs
@@ -12,7 +12,7 @@ namespace Probrem8_6
         {
             Random rnd = new Random();
             int rnd_n_min = 1;
-            int rnd_d_min = 2;
+            int rnd_d_min = 1;
             int rnd_max = 10;
 
             Fraction f1 = new Fraction();
@@ -24,6 +24,7 @@ namespace Probrem8_6
 
             // 2つの分数の足し算
             Fraction f = AddFraction(f1, f2);
+
             // 計算結果表示
             Console.WriteLine("{0} + {1} = {2}", f1.GetFracString(), f2.GetFracString(), f.GetFracString());
         }
@@ -31,9 +32,11 @@ namespace Probrem8_6
         static Fraction AddFraction(Fraction f1, Fraction f2)
         {
             Fraction f = new Fraction();
+            f.I = f1.I + f2.I; // 整数部分の足し算
             f.N = (f1.N * f2.D) + (f2.N * f1.D); // 通分後の分子の足し算
             f.D = f1.D * f2.D; // 通分後の分母
-            f.AboutAndBand(); // 約分と帯分数化
+            f.Band();
+            f.About();
             return f;
         }
     }
